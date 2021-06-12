eef1b9d5774aeb63b9769c4688a48281e056754f
Viktoria Alekseeva <Yourlexss@gmail.com>
2021-06-12 10:43:06 +0300

    added js,scss,html,hbs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './sass/main.scss';
+import eventApiService from './js/apiService';
+import modal from './js/modal';
+import searchForm from './js/search-forms';
+
+import modalTpl from './temlates/modal.hbs';
+import eventTpl from './temlates/event.hbs';
+
