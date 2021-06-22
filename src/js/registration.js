9d91038b2cd52f19b8b2d01a212b4ecdcebba178
Nastya-Lopatina <nastya.lopatina.93@gmail.com>
2021-06-22 16:50:35 +0900

    делаю регистрацию

--- src/js/registration.js
+++ src/js/registration.js
@@ -1,34 +1,36 @@
 
-function userRegistration(user,phone,email){
-    const url = `http://localhost:8080/addUser?user=${user}&phone=${phone}&email=${email}`;
-  
-  
-  fetch(url).then((response) => {
-   //console.log('text ' , response);
-   if (response.status == 200) {
-     console.log('ok');
-   }
-  
-   if (response.status == 400) {
-     console.log('BAD REQUEST');
-     
-   }
-   response.json().then((data) => {
-       console.log(data.value);
-       
-   });
-  });
-  }
+async function userRegistration(user,phone,email){
+  const url = `http://localhost:8080/addUser?user=${user}&phone=${phone}&email=${email}`;
+
+try {
+  await fetch(url).then((response) => {
+ //console.log('text ' , response);
+ if (response.status == 200) {
+   console.log('ok');
+ }
+
+ if (response.status == 400) {
+   console.log('BAD REQUEST');
+ }
+ response.json().then((data) => {
+     console.log(data);
+ });
+});
+} catch (error) {
+  alert('Такой пользователь уже есть');
+}
+}
   
   const form = document.getElementById('form')
   const button = document.getElementById ('button-submit')
   
-  //form.addEventListener('', checkForFilling)
-  button.addEventListener('click',buttonPrevent)
+  form.addEventListener('submit', checkForFilling)
+  button.addEventListener('click', onFormSubmit )
   
   //  функия для отправки формы
-  function buttonPrevent (e){
+  function onFormSubmit (e){
     e.preventDefault();
+
     const user = document.getElementById('user').value
     const phone = document.getElementById('phone').value
     const email = document.getElementById('email').value
@@ -36,8 +38,9 @@ function userRegistration(user,phone,email){
     if (checkForFilling(user,phone,email)) {
       userRegistration(user,phone,email)
     }
-  
-    console.log(e)
+     
+    e.currentTarget.reset()
+   
   }
   
   
@@ -47,19 +50,15 @@ function checkForFilling (user,phone,email) {
   if (user === '') {
      alert('Ведите имя');
     return false;
-   
-    }
-    if (phone === '') {
+  }
+  if (phone === '') {
       alert('Ведите телефон');
      return false;
-      
-     }
-  
-     if (email === '') {
+  }
+  if (email === '') {
       alert('Ведите email');
      return false;
-      
-     }
+  }
    else {
     return true;
     
